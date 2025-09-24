e7c28acdb8cb3c0f15f01c8879cbd10fdee0171f
JoaoMotaSilva10 <joaomotasilva10@outlook.com>
2025-09-24 20:01:11 -0300

    retirando imports desnecessários

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import '../model/usuario.dart';
 import '../model/reserva.dart';
 import '../model/recurso.dart';
 
