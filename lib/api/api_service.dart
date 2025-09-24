744d6daa79628b044b3c48a2f2d59df3cd6a6294
JoaoMotaSilva10 <joaomotasilva10@outlook.com>
2025-09-24 16:19:19 -0300

    manutenção para rodar no emulador

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,11 +1,21 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../model/usuario.dart';
 import '../model/reserva.dart';
 import '../model/recurso.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:8080';
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080'; // ou 'http://10.14.206.190:8080'
+    } else {
+      return 'http://localhost:8080';
+    }
+  }
 
   // LOGIN
   static Future<http.Response> login(String email, String senha) async {
