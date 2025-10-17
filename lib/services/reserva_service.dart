fe85afee8a43e7f24ae256e629461ce1985ae2e4
JoaoMotaSilva10 <joaomotasilva10@outlook.com>
2025-10-17 10:23:42 -0300

    fix: Update base URL to use 10.0.2.2 for API services

--- lib/services/reserva_service.dart
+++ lib/services/reserva_service.dart
@@ -5,7 +5,7 @@ import '../model/recurso.dart';
 import 'auth_service.dart';
 
 class ReservaService {
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'http://10.0.2.2:8080';
 
   // Criar nova reserva
   static Future<Reserva> criarReserva({
